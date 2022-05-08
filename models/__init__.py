ee02e2b72a8fea83a70caabbfe25c292d9bcbe34
Neeraj Gangwar <y.neeraj2008@gmail.com>
2022-05-07 21:42:38 -0500

    Delete original code

--- models/__init__.py
+++
@@ -1,9 +0,0 @@
-from .basemodel import BaseModel
-from .vtnetmodel import VTNetModel, VisualTransformer
-from .pretrainedvisualtransformer import PreTrainedVisualTransformer
-
-__all__ = [
-    'BaseModel', 'VTNetModel', 'VisualTransformer', 'PreTrainedVisualTransformer',
-]
-
-variables = locals()
--- optimizers/__init__.py
+++
@@ -1,9 +0,0 @@
-from .shared_RMS_prop import SharedRMSprop
-from .shared_adam import SharedAdam
-
-__all__ = [
-    'SharedRMSprop',
-    'SharedAdam',
-]
-
-variables = locals()
